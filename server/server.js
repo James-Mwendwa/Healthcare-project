99783c213da7df2313c7fd7740f16584336ed08e
James Mwendwa <mwendwajames32@gmail.com>
2023-03-21 20:37:55 +0300

    connect server to db

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 //create express app
 const app = express();
 
+dotenv.config();
+
 //connect to database
+mongoose.connect(process.env.CONNECT_DB);
 
 //middleware
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server is up");
